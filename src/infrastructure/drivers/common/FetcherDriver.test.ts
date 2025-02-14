f7a6c8c3b9a98610d502ee7a63bdfbd6fbb526e5
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-02-14 18:56:59 +0100

    fix(Fetcher): fix broken tests

--- src/infrastructure/drivers/common/FetcherDriver.test.ts
+++ src/infrastructure/drivers/common/FetcherDriver.test.ts
@@ -5,7 +5,7 @@ const fetcher = new FetcherDriver()
 
 describe('get', () => {
   it('should make a GET request and return a response', async () => {
-    const response = await fetcher.get('https://example.com')
+    const response = await fetcher.get('https://latechforce.com')
     const data = await response.text()
 
     expect(response.status).toBe(200)
@@ -16,7 +16,7 @@ describe('get', () => {
 describe('post', () => {
   it('should make a POST request with correct headers and body', async () => {
     const requestBody = { name: 'Test' }
-    const response = await fetcher.post('https://example.com', requestBody)
+    const response = await fetcher.post('https://latechforce.com', requestBody)
     const data = await response.text()
 
     expect(response.status).toBe(403)
