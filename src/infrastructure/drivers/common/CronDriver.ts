8d7c50535ea5dcbf49b0985f01f7763af4aa91cc
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-02-17 12:07:59 +0100

    chore(Cron): add cron driver

---
+++ src/infrastructure/drivers/common/CronDriver.ts
@@ -0,0 +1,22 @@
+import { CronJob } from 'cron'
+import { nanoid } from 'nanoid'
+import type { ICronDriver } from '/adapter/spi/drivers/CronDriverSpi'
+
+export class CronDriver implements ICronDriver {
+  private _jobs = new Map<string, CronJob>()
+
+  start(cronTime: string, onTick: () => void): string {
+    const id = nanoid()
+    const job = new CronJob(cronTime, onTick, null, true, 'Europe/Paris')
+    this._jobs.set(id, job)
+    return id
+  }
+
+  stop(id: string): void {
+    const job = this._jobs.get(id)
+    if (job) {
+      job.stop()
+      this._jobs.delete(id)
+    }
+  }
+}
