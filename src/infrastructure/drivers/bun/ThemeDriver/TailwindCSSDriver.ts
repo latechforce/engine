4fd2765c7240cd274512cc595818fa97512b4297
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-03-26 18:33:03 +0100

    fix(Theme): add tailwindcss style import from npm package

--- src/infrastructure/drivers/bun/ThemeDriver/TailwindCSSDriver.ts
+++ src/infrastructure/drivers/bun/ThemeDriver/TailwindCSSDriver.ts
@@ -12,6 +12,7 @@ export class TailwindCSSDriver implements IThemeDriver {
     const input = `
       @import "tailwindcss";
       @import "preline/variants.css";
+      @source "../node_modules/@latechforce/engine";
 
       @plugin "@tailwindcss/forms";
     `
@@ -20,6 +21,8 @@ export class TailwindCSSDriver implements IThemeDriver {
     await fs.writeFile(inputPath, input)
     await $`bunx @tailwindcss/cli -i ${inputPath} -o ${outputPath}`.quiet()
     const output = await fs.readFile(outputPath, 'utf8')
+    await fs.unlink(inputPath)
+    await fs.unlink(outputPath)
     return output
   }
 
