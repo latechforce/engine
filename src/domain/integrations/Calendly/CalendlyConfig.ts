2275f2e84bfd7e49c760664af9dce64a4ab92fcd
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-04-01 16:15:19 +0200

    chore: remove useless code

--- src/domain/integrations/Calendly/CalendlyConfig.ts
+++ src/domain/integrations/Calendly/CalendlyConfig.ts
@@ -3,10 +3,6 @@ export type CalendlyConfig = {
    * Personal access token for Calendly API authentication
    * See: https://developer.calendly.com/getting-started
    */
-  client: {
-    id: string
-    secret: string
-  }
   user: {
     accessToken: string
   }
