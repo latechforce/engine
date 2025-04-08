e9f45b446b9d082fe60ec93f9589a55dcd01bb74
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-04-08 11:39:27 +0200

    fix(CI°: add env variables

--- src/infrastructure/integrations/common/calendly/CalendlyIntegration.test.ts
+++ src/infrastructure/integrations/common/calendly/CalendlyIntegration.test.ts
@@ -3,9 +3,10 @@ import { testCalendlyIntegration } from './CalendlyIntegrationTest'
 import env from '/infrastructure/test/env'
 import BunTester from 'bun:test'
 
-const { TEST_CALENDLY_USER_ACCESS_TOKEN } = env
+const { TEST_CALENDLY_BASE_URL, TEST_CALENDLY_USER_ACCESS_TOKEN } = env
 
 export const integration = new CalendlyIntegration({
+  baseUrl: TEST_CALENDLY_BASE_URL,
   user: {
     accessToken: TEST_CALENDLY_USER_ACCESS_TOKEN,
   },
