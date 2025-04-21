49b7abb319cef1f17dc9e62ce750b31a580cb129
Thomas Stocker <thomas.stocker.pro@gmail.com>
2025-04-22 00:14:04 +0700

    chore(examples): update some examples with server port and automations

--- examples/config/automation/trigger/integration/jotform/formWebhookReceived/index.ts
+++ examples/config/automation/trigger/integration/jotform/formWebhookReceived/index.ts
@@ -2,16 +2,30 @@ import type { Config } from '/src'
 
 export const configAutomationTriggerIntegrationJotformFormWebhookReceived: Config = {
   name: 'App with an automation with a jotform form webhook received trigger',
+  integrations: {
+    jotform: [
+      {
+        account: 'jotform_account',
+        apiKey: '{{ env.TEST_JOTFORM_API_KEY }}',
+        baseUrl: '{{ env.TEST_JOTFORM_BASE_URL }}',
+      },
+    ],
+  },
   automations: [
     {
       name: 'FormWebhookReceived',
       trigger: {
         integration: 'Jotform',
         event: 'FormWebhookReceived',
-        account: 'jotform',
-        formId: '{{ env.JOTFORM_FORM_ID }}',
+        account: 'jotform_account',
+        formId: '{{ env.TEST_JOTFORM_FORM_ID }}',
       },
       actions: [],
     },
   ],
+  services: {
+    server: {
+      port: 3000,
+    },
+  },
 }
