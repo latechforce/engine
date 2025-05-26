91d5d519941ef21a5e4a196cd653e85a36f5cd9a
thomas-jeanneau <thomas.jeanneau@latechforce.com>
2025-05-26 08:08:34 +0000

    Deploy website - based on 1dd17cacf4e527adb477b0b6b2242ab3d6857787

--- assets/js/814f3328.f92a7e1d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Release 0.47.0","permalink":"/blog/release-0-47-0","unlisted":false,"date":"2025-05-22T00:00:00.000Z"}]}')}}]);
