bdc98b9af05d7b79aa2625eacce844abe58677be
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-08-19 09:38:39 +0200

    fix: setup trigger when automation is set active

--- src/features/automation/application/use-case/set-status.use-case.ts
+++ src/features/automation/application/use-case/set-status.use-case.ts
@@ -1,14 +1,17 @@
 import { inject, injectable } from 'inversify'
 import type { App } from '../../../app/domain/entity/app.entity'
-import TYPES from '../di/types'
+import TYPES from '../../../../shared/application/di/types'
 import type { IAutomationRepository } from '../../domain/repository-interface/automation-repository.interface'
 import { HttpError } from '../../../../shared/domain/entity/http-error.entity'
+import type { SetupTriggerUseCase } from '../../../../features/trigger/application/use-case/setup-trigger.use-case'
 
 @injectable()
 export class SetStatusUseCase {
   constructor(
-    @inject(TYPES.Repository)
-    private readonly automationRepository: IAutomationRepository
+    @inject(TYPES.Automation.Repository)
+    private readonly automationRepository: IAutomationRepository,
+    @inject(TYPES.Trigger.UseCase.Setup)
+    private readonly setupTriggerUseCase: SetupTriggerUseCase
   ) {}
 
   async execute(app: App, automationId: string, active: boolean): Promise<void> {
@@ -17,5 +20,8 @@ export class SetStatusUseCase {
       throw new HttpError('Automation not found', 404)
     }
     await this.automationRepository.status.setActive(automation.schema.id, active)
+    if (active) {
+      await this.setupTriggerUseCase.execute(app, automation)
+    }
   }
 }
